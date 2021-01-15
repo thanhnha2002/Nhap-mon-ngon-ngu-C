70fb396e6f16e7bb608f203dfd547aa7041af52a
thanhnha <72533406+thanhnha2002@users.noreply.github.com>
2021-01-15 07:54:59 +0700

    Create kt_01.c

---
+++ Bài tập 3/kt_01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+int a, b, c;
+  a = 2, b = 3, c = 4;
+  //hien thi ra man hinh BAN CUU CHUONG
+  printf("\t                         BANG CUU CHUONG\n");
+  //hien thi BAN CUU CHUONG 2 3 4 , 3 dong dau
+  printf("BANG CUU CHUONG 2\n");
+  printf("%d x 1 = %d\n", a, a * 1);
+  printf("%d x 2 = %d\n", a, a * 2);
+  printf("%d x 3 = %d\n", a, a * 3);
+
+  printf("BANG CUU CHUONG 3\n");
+  printf("%d x 1 = %d\n", b, b * 1);
+  printf("%d x 2 = %d\n", b, b * 2);
+  printf("%d x 3 = %d\n", b, b * 3);
+
+  printf("BANG CUU CHUONG 4\n");
+  printf("%d x 1 = %d\n", c, c * 1);
+  printf("%d x 2 = %d\n", c, c * 2);
+  printf("%d x 3 = %d\n", c, c * 3);
+
+    return 0; 
+}
