0d8b6ba8d2aca09753078cfde1b6cd45b783607d
thanhnha <72533406+thanhnha2002@users.noreply.github.com>
2021-01-21 08:39:39 +0700

    Create mang_02.c

---
+++ Bài tập 4/mang_02.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap a = ");
+	scanf("%d", &n);
+  
+  int b[n];
+	for(int c = 0;c < n; c++){
+    printf("Nhap phan tu thu %d = ", c + 1);
+	scanf("%d", &a[i]);
+  }
+   printf("mang a: ");
+  for(int d = 0;d < n; d++){
+    printf(" %d ", a[i]);
+    
+  }
+  for(int i = 0;i < n; i++){
+  if (a[i] % 2 == 0) {
+      printf("\n%d la so chan\n", a[i]);
+  }
+  }
+     return 0;}
